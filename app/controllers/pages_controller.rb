500a8422d28b9b8dd7dd43db7cdc41426d0158eb
txoxu <king.crimson05@outlook.jp>
2024-05-16 23:59:38 +0900

    コミット

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def mypage
-  end
+  def mypage; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/mypage'
+  get 'home', to: 'pages#home'
+  get 'mypage', to: 'pages#mypage'
+
+  # ルートをホームページに設定
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
