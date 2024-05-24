e4ff9f03a9755595ac3b8fc09fb6309da6610ba6
txoxu <king.crimson05@outlook.jp>
2024-05-24 23:37:45 +0900

    preview.jsを作成しました

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,14 +4,3 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 
 
-document.getElementById('image_input').addEventListener('change', function (e) {
-  // 1枚だけ表示する
-  var file = e.target.files[0];
-
-  // ファイルのブラウザ上でのURLを取得する
-  var blobUrl = window.URL.createObjectURL(file);
-
-  // img要素に表示
-  var img = document.getElementById('image_preview');
-  img.src = blobUrl;
-});
