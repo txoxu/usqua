e6f56493ac926e44c786a0311588afe948836ab2
txoxu <king.crimson05@outlook.jp>
2024-05-25 10:57:55 +0900

    プレビュー機能実装中

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_post');
+  const postForm = document.getElementById('new_whiskey');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
 
   // input要素を取得。name情報は検証ツールから確認すること！！
-  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+  const fileField = document.querySelector('input[type="file"][name="whiskey[image]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
   // 古いプレビューが存在する場合は削除(最後に記述する)
