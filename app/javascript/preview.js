415d01403c0ee528a053a8f2925a1c4be90f6402
txoxu <king.crimson05@outlook.jp>
2024-05-25 22:14:17 +0900

    プレビュー機能実装中

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_whiskey');
+  const whiskeyForm = document.getElementById('new_whiskey');
   // プレビューを表示するためのスペースを取得
-  const previewList = document.getElementById('previews');
+  const previewList = document.querySelector('.image-container #previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
-  if (!postForm) return null;
+  if (!whiskeyForm) return null;
 
   // input要素を取得。name情報は検証ツールから確認すること！！
   const fileField = document.querySelector('input[type="file"][name="whiskey[image]"]');
