bfec09bfb07beaba357fafa0e481ad51198b4e92
txoxu <king-crimson05@outlook.jp>
2024-06-08 23:34:26 +0900

    マイページレイアウト完了

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import "chartkick"
-import "Chart.bundle"
 import "chartkick/chart.js"
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,4 @@
 pin "application", to: "application.js"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+
