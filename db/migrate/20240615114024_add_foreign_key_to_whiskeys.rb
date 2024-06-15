4397a458df0447925edcfd189c42ac366d898055
txoxu <king-crimson05@outlook.jp>
2024-06-15 23:27:17 +0900

    外部キーを追加

--- db/migrate/20240615114024_add_foreign_key_to_whiskeys.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToWhiskeys < ActiveRecord::Migration[7.1]
-  def change
-    add_foreign_key :whiskeys, :users
-  end
-end
---
+++ db/migrate/20240615141413_add_user_id_to_whiskeys.rb
@@ -0,0 +1,5 @@
+class AddUserIdToWhiskeys < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :whiskeys, :user, null: false, foreign_key: true
+  end
+end
