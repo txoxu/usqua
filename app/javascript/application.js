7d6cfe32f348add0b1fbb544a227511e936949d7
txoxu <king-crimson05@outlook.jp>
2024-06-23 10:21:23 +0900

    変更しました

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-<<<<<<< HEAD
 import "chartkick/chart.js"
-=======
->>>>>>> 81dd2749b47c942904553ec36883c554e458af9b
