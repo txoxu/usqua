7f1b727ea7c324d536567bf4c5b840178fd5d8e4
txoxu <king-crimson05@outlook.jp>
2024-06-24 19:45:09 +0900

    再構築成功

--- db/migrate/20240617092608_remove_remmaining_quantity_from_whiskeys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRemmainingQuantityFromWhiskeys < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :whiskeys, :remmaining_quantity, :integer
-  end
-end
--- db/migrate/20240619111803_add_quantity_image_to_remmaining_quantities.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityImageToRemmainingQuantities < ActiveRecord::Migration[7.1]
-  def change
-    add_column :remmaining_quantities, :quantity_image, :string
-  end
-end
