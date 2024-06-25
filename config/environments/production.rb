da0525efd506fba6dc01ba875fcf8e7203977b8b
txoxu <king-crimson05@outlook.jp>
2024-06-26 02:09:48 +0900

    変更しました

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.action_controller.asset_host = https://usqua-b00a6d1e8961.herokuapp.com/
+  config.action_controller.asset_host = "https://usqua-b00a6d1e8961.herokuapp.com/"
   config.assets.compile = false
-
+  config.assets.digest = true
+config.assets.precompile += %w( .svg .eot .woff .ttf .jpg .png .gif )
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
