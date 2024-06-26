c42c1d8b66cdf09d313e53e25fcf5b9d8050e84a
txoxu <king-crimson05@outlook.jp>
2024-06-26 23:01:34 +0900

    動作確認修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def convert_quantity_image_filename(filename)
+    # 例: '200ml-60b110b7cc6a0164de1821f3905805f5f52725a119106913b914dd503db87032.jpg' を '200ml.jpg' に変換
+    filename.gsub(/-\w+\.jpg$/, '.jpg')
+  end
 end
