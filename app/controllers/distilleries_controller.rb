1c463e72e7ba0d57d02f2c95c14025dad12764fd
txoxu <king-crimson05@outlook.jp>
2024-08-12 00:02:02 +0900

    検索機能削除

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,16 +1,9 @@
 class DistilleriesController < ApplicationController
   def index
     @regions = Region.all
-    @distillery_search_form =SearchDistilleriesForm.new(search_params)
-    @distilleries = @distillery_search_form.search
+    @distilleries = Distillery.all
   end
 
   def show
-  end
-
-  private
-
-  def search_params
-    params.fetch(:search_distilleries_form, {}).permit(:keyword)
   end
 end
--- app/models/distillery.rb
+++ app/models/distillery.rb
@@ -1,5 +1,3 @@
 class Distillery < ApplicationRecord
   belongs_to :region
-
-  scope :keyword, -> (word) { where('distillery_name LIKE ? or prefectures LIKE ?', "%#{word}%", "%#{word}%") }
 end
