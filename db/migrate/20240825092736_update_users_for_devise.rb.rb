ca9bb9000be48674550c6b834c93f78f79e9adb2
txoxu <king-crimson05@outlook.jp>
2024-09-09 03:03:05 +0900

    変更

--- db/migrate/20240825092736_update_users_for_devise.rb.rb
+++ db/migrate/20240825092736_update_users_for_devise.rb.rb
@@ -1,17 +1,18 @@
 class UpdateUsersForDevise < ActiveRecord::Migration[7.1]
-   def change
+  def up
     # カラムが存在すれば削除
     remove_column :users, :salt if column_exists?(:users, :salt)
     remove_column :users, :crypted_password if column_exists?(:users, :crypted_password)
     remove_column :users, :first_name if column_exists?(:users, :first_name)
     remove_column :users, :last_name if column_exists?(:users, :last_name)
-    
+
     # カラムが存在しなければ追加
     add_column :users, :name, :string, null: false, default: "" unless column_exists?(:users, :name)
     add_column :users, :encrypted_password, :string, default: "" unless column_exists?(:users, :encrypted_password)
     add_column :users, :reset_password_token, :string unless column_exists?(:users, :reset_password_token)
     add_column :users, :reset_password_sent_at, :datetime unless column_exists?(:users, :reset_password_sent_at)
     add_column :users, :remember_created_at, :datetime unless column_exists?(:users, :remember_created_at)
+
     # インデックスの存在を確認し、必要であれば追加
     unless index_exists?(:users, :email, unique: true)
       add_index :users, :email, unique: true
@@ -20,10 +21,27 @@ class UpdateUsersForDevise < ActiveRecord::Migration[7.1]
     unless index_exists?(:users, :reset_password_token, unique: true)
       add_index :users, :reset_password_token, unique: true
     end
+  end
+
+  def down
+    # ロールバック時の処理
+    remove_column :users, :name if column_exists?(:users, :name)
+    remove_column :users, :encrypted_password if column_exists?(:users, :encrypted_password)
+    remove_column :users, :reset_password_token if column_exists?(:users, :reset_password_token)
+    remove_column :users, :reset_password_sent_at if column_exists?(:users, :reset_password_sent_at)
+    remove_column :users, :remember_created_at if column_exists?(:users, :remember_created_at)
+
+    add_column :users, :salt, :string unless column_exists?(:users, :salt)
+    add_column :users, :crypted_password, :string unless column_exists?(:users, :crypted_password)
+    add_column :users, :first_name, :string unless column_exists?(:users, :first_name)
+    add_column :users, :last_name, :string unless column_exists?(:users, :last_name)
+
+    if index_exists?(:users, :email, unique: true)
+      remove_index :users, :email
+    end
 
     if index_exists?(:users, :reset_password_token, unique: true)
-      remove_index :users, :reset_password_token, unique: true
+      remove_index :users, :reset_password_token
     end
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -122,12 +122,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_25_124630) do
     t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.string "encrypted_password", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "whiskey_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
