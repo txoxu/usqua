4800bae3df5784043cbfe26d48748ccf8f4514fa
txoxu <king-crimson05@outlook.jp>
2024-09-18 01:51:54 +0900

    お問い合わせ機能追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,6 +94,15 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'usqua-b00a6d1e8961.herokuapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      user_name: ENV['GOOGLE_MAIL_ADDRESS'],
+      password: ENV['GOOGLE_APP_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
 end
