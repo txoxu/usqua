5c9bab0556f54df08862a955f171acf8bebbf427
txoxu <king-crimson05@outlook.jp>
2024-09-24 02:34:07 +0900

    user_providerのuser_idのnull falseを削除

---
+++ db/migrate/20240923172937_change_user_id_null_on_user_providers.rb
@@ -0,0 +1,5 @@
+class ChangeUserIdNullOnUserProviders < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :user_providers, :user_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_18_191950) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_23_172937) do
   create_table "base_cocktails", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "base_name", null: false
     t.datetime "created_at", null: false
@@ -117,7 +117,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_18_191950) do
   end
 
   create_table "user_providers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.string "provider"
     t.string "uid"
     t.datetime "created_at", null: false
