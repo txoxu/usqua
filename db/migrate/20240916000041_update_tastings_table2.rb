89a2d717c0160c11077fa5fbe1083affb5e71f27
txoxu <king-crimson05@outlook.jp>
2024-10-06 17:45:08 +0900

    変更

--- db/migrate/20240916000041_update_tastings_table2.rb
+++ db/migrate/20240916000041_update_tastings_table2.rb
@@ -2,13 +2,23 @@
 
 # テイスティングテーブルの文字数を変更
 class UpdateTastingsTable2 < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_table :tastings, bulk: true do |t|
       t.change :aroma, precision: 4, scale: 2
       t.change :flavor, precision: 4, scale: 2
       t.change :body, precision: 4, scale: 2
       t.change :finish, precision: 4, scale: 2
       t.change :balance, precision: 4, scale: 2
+    end
+  end
+
+  def down
+    change_table :tastings, bulk: true do |t|
+      t.change :aroma, :decimal
+      t.change :flavor, :decimal
+      t.change :body, :decimal
+      t.change :finish, :decimal
+      t.change :balance, :decimal
     end
   end
 end
