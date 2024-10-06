07ce20759881a23a4fc06b078a58f59a6c32606c
txoxu <king-crimson05@outlook.jp>
2024-10-06 17:37:28 +0900

    変更

--- db/migrate/20240916000041_update_tastings_table2.rb
+++ db/migrate/20240916000041_update_tastings_table2.rb
@@ -4,11 +4,11 @@
 class UpdateTastingsTable2 < ActiveRecord::Migration[7.1]
   def change
     change_table :tastings, bulk: true do |t|
-      t.decimal :aroma, precision: 4, scale: 2
-      t.decimal :flavor, precision: 4, scale: 2
-      t.decimal :body, precision: 4, scale: 2
-      t.decimal :finish, precision: 4, scale: 2
-      t.decimal :balance, precision: 4, scale: 2
+      t.change :aroma, precision: 4, scale: 2
+      t.change :flavor, precision: 4, scale: 2
+      t.change :body, precision: 4, scale: 2
+      t.change :finish, precision: 4, scale: 2
+      t.change :balance, precision: 4, scale: 2
     end
   end
 end
