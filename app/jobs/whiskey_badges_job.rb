60e1701208b04d5cdff21abe24213de2f6ef4272
txoxu <king-crimson05@outlook.jp>
2024-10-20 01:37:53 +0900

    rubocopの修正改善

--- app/jobs/whiskey_badges_job.rb
+++ app/jobs/whiskey_badges_job.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# ウイスキーの称号ロジック
 class WhiskeyBadgesJob < ApplicationJob
   queue_as :default
 
@@ -7,16 +10,11 @@ class WhiskeyBadgesJob < ApplicationJob
 
     user = User.find(user_id)
     whiskey = Whiskey.find(whiskey_id)
-    Rails.logger.info "User found: #{user.id}, Whiskey found: #{whiskey.id}"
 
     WhiskeyBadge.all.each do |badge|
       conditions = badge.conditions
-      Rails.logger.info "Evaluating badge: #{badge.name} with conditions: #{conditions}"
       if meets_conditions?(conditions, whiskey, user)
         user.assign_badge(badge)
-        Rails.logger.info "Badge assigned: #{badge.name} to user: #{user.id}"
-      else
-        Rails.logger.info "Badge not assigned: #{badge.name}"
       end
     end
   end
@@ -26,7 +24,7 @@ class WhiskeyBadgesJob < ApplicationJob
   def meets_conditions?(conditions, _whiskey, user)
     case conditions['type']
 
-    when "#{whiskey_badge.conditions['type']}"
+    when 'new_whiskey_count'
       user.whiskeys.count == conditions['count']
     when 'ten_whiskey_count'
       user.whiskeys.count == conditions['count']
--- db/migrate/20241019162030_updatecocktails.rb
+++ db/migrate/20241019162030_updatecocktails.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# cocktailsテーブルにcocktail_imageカラムを追加
 class Updatecocktails < ActiveRecord::Migration[7.1]
   def change
     add_column :cocktails, :cocktail_image, :string
