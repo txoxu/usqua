03f9f1ae7ca802494ad9c5144507437b5b138ab2
txoxu <king-crimson05@outlook.jp>
2024-11-22 12:44:51 +0900

    rubocop 自動修正

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,5 +2,5 @@
 
 # 蒸溜所を分ける地方のテーブル
 class Region < ApplicationRecord
-  has_many :distilleries
+  has_many :distilleries, dependent: :destroy
 end
--- db/migrate/20241120151140_add_region_to_distilleries.rb
+++ db/migrate/20241120151140_add_region_to_distilleries.rb
@@ -3,7 +3,7 @@
 # 1対多の関係で外部キーを設定
 class AddRegionToDistilleries < ActiveRecord::Migration[7.1]
   def change
-    unless column_exists?(:distilleries, :region_id)
+    return if column_exists?(:distilleries, :region_id)
       add_column :distilleries, :region_id, :integer
     end
   end
