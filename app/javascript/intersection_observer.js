acf3479b45e344ed8029e995590fa1c163a025e8
txoxu <king-crimson05@outlook.jp>
2024-11-26 20:55:04 +0900

    topページのスクリーンショットの部分をintersectionObserverを使用して動的に表示
    次、一番下のログイン、新規登録ボタンを動的に表示

--- app/javascript/intersection_observer.js
+++ app/javascript/intersection_observer.js
@@ -1,4 +1,28 @@
 //監視対象が範囲に入ったら動作
-const showMypage = () => {
-  console.log('キリンさんです');
-}
+const showMypage = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0', 0],
+  };
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+    entry.target.animate(keyframes, 600); // 監視対象ごとにアニメーションを適用
+    observer.unobserve(entry.target);
+    }
+  });
+};
+
+//オプション設定
+const options = {
+  threshold: 0.1
+};
+
+//監視ロボットの設定
+const mypageObserver =  new IntersectionObserver(showMypage, options);
+
+//監視するように指示
+mypageObserver.observe(document.querySelector('#mypage'));
+mypageObserver.observe(document.querySelector('#new-whiskey'));
+mypageObserver.observe(document.querySelector('#index-cocktail'));
+mypageObserver.observe(document.querySelector('#distillery'));
+
