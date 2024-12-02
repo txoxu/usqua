40ecda9f7c4b6a87fe72ce395d0069a6ca3190f0
txoxu <king-crimson05@outlook.jp>
2024-12-03 00:04:59 +0900

    カクテル一覧にアニメーションを加えました

--- app/javascript/dark_mode.js
+++ app/javascript/dark_mode.js
@@ -1,11 +1,25 @@
-const btn = document.querySelector('#btn');
+document.addEventListener('turbo:load', function() {
+  const btn = document.querySelector('#btn');
 
-btn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-
-  if (btn.textContent === 'ダークモード') {
+  // ページが読み込まれた時にローカルストレージから状態を読み込む
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
     btn.textContent = 'ライトモード';
   } else {
+    document.body.classList.remove('dark-theme');
     btn.textContent = 'ダークモード';
   }
+
+  btn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    // ダークモードの状態をローカルストレージに保存
+    if (document.body.classList.contains('dark-theme')) {
+      localStorage.setItem('theme', 'dark');
+      btn.textContent = 'ライトモード';
+    } else {
+      localStorage.setItem('theme', 'light');
+      btn.textContent = 'ダークモード';
+    }
+  });
 });
